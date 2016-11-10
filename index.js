7ad03a5c676267c8e393bdf6825a763004f9da32
Thomas Bernfeld <happpyier@gmail.com>
2016-11-10 10:21:37 -0500

    Yelpier Data

--- index.js
+++ index.js
@@ -33,6 +33,12 @@ var twitter = new Twitter({
 	consumerSecret: 'UxkG05HcRBlOmOVLvcHM9AlFStHStUMKwtuCKXM0nwtbm5IJAP',
 	callback: 'https://happpypr.herokuapp.com/windowClose'
 });
+var yelpApp = new yelp({
+	consumer_key: '7dAg-Gi0XU4GQK1pl-YSNw',
+	consumer_secret: 'HOg3M2ussnUbXMFY2Q3mBsrdrmo',
+	token: 'WPM0LWSJtD0y3C6kqYDFpnjIYSVB--7Z',
+	token_secret: 'GRHNx-coBSsG_wFboFlw1mhX6KU'
+});
 app.set('port', (process.env.PORT || 5000));
 app.use(cookieParser());
 app.set("Content-Type", "text/html");
@@ -49,18 +55,18 @@ app.get([''], function(request, response)
 			response.write(data);
 			
 		});
-		var options = 
-		{
-			consumer_key: '7dAg-Gi0XU4GQK1pl-YSNw',
-			consumer_secret: 'HOg3M2ussnUbXMFY2Q3mBsrdrmo',
-			token: 'WPM0LWSJtD0y3C6kqYDFpnjIYSVB--7Z',
-			token_secret: 'GRHNx-coBSsG_wFboFlw1mhX6KU',
-		};
-		var parameters = 
-		{
-			term: 'restaurant',
-			location: '33611',
-		};
+		// var options = 
+		// {
+			// consumer_key: '7dAg-Gi0XU4GQK1pl-YSNw',
+			// consumer_secret: 'HOg3M2ussnUbXMFY2Q3mBsrdrmo',
+			// token: 'WPM0LWSJtD0y3C6kqYDFpnjIYSVB--7Z',
+			// token_secret: 'GRHNx-coBSsG_wFboFlw1mhX6KU',
+		// };
+		// var parameters = 
+		// {
+			// term: 'restaurant',
+			// location: '33611',
+		// };
 		// yelp.search(merge(options, parameters), (data) => {
 		// console.log(data);
 		// }, (err) => {
