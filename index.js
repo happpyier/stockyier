0bc68c0fe6152bc4d8e79080e4e07ddbc0c722bc
Thomas Bernfeld <happpyier@gmail.com>
2016-11-10 10:00:47 -0500

    Yelpier Data

--- index.js
+++ index.js
@@ -28,18 +28,18 @@ var clientUser;
 var clientUser;
 var pre_clientUser;
 var Almost_clientUser;
-// var options = 
-// {
-	// consumer_key: '7dAg-Gi0XU4GQK1pl-YSNw',
-	// consumer_secret: 'HOg3M2ussnUbXMFY2Q3mBsrdrmo',
-	// token: 'WPM0LWSJtD0y3C6kqYDFpnjIYSVB--7Z',
-	// token_secret: 'GRHNx-coBSsG_wFboFlw1mhX6KU'
-// };
-// var parameters = 
-// {
-	// term: 'restaurant',
-	// location: '33611'
-// };
+var options = 
+{
+	consumer_key: '7dAg-Gi0XU4GQK1pl-YSNw',
+	consumer_secret: 'HOg3M2ussnUbXMFY2Q3mBsrdrmo',
+	token: 'WPM0LWSJtD0y3C6kqYDFpnjIYSVB--7Z',
+	token_secret: 'GRHNx-coBSsG_wFboFlw1mhX6KU',
+};
+var parameters = 
+{
+	term: 'restaurant',
+	location: '33611',
+};
 var twitter = new Twitter({
 	consumerKey: 'YZoBVI9Ak2MAxLTRJ460c65Oq',
 	consumerSecret: 'UxkG05HcRBlOmOVLvcHM9AlFStHStUMKwtuCKXM0nwtbm5IJAP',
@@ -59,7 +59,15 @@ app.get([''], function(request, response)
 				return console.log(err);
 			}
 			response.write(data);
-			response.write("Yelp Data");
+			yelp.search(merge(options, parameters), function(err, dataYelp) 
+			{
+				if (err)
+				{
+					return console.log(err);
+				}
+				response.write(dataYelp);
+			});
+			
 		});
 		fs.readFile('indexSignedIn2.html', 'utf8', function (err,data) 
 		{
