1b137419db50ab264d1e8ebc60e763e4aef97f75
Thomas Bernfeld <happpyier@gmail.com>
2016-11-10 10:34:00 -0500

    Yelpier Data

--- index.js
+++ index.js
@@ -53,13 +53,13 @@ app.get([''], function(request, response)
 				return console.log(err);
 			}
 			response.write(data);
-			
-		});
-		yelp.search({ term: 'restaurant', location: '33611' }).then(function (data) 
-		{
+			yelp.search({ term: 'restaurant', location: '33611' }).then(function (data) 
+			{
 			//response.write(JSON.stringify(data));
 			response.write("it got here!!!!!!!!!!!!");
+			});
 		});
+
 		fs.readFile('indexSignedIn2.html', 'utf8', function (err,data) 
 		{
 			if (err) 
