f0b5fa89b5def8b70111ffc4357734c364b16c78
Thomas Bernfeld <happpyier@gmail.com>
2016-11-11 10:49:15 -0500

    Yelpier Data Stufs

--- index.js
+++ index.js
@@ -69,7 +69,7 @@ app.get([''], function(request, response)
 			_count = businesses.length;
 			//businesses = JSON.stringify(Object.keys(prebusinesses));
 			//locationVal = typeof(JSON.stringify(location));
-			_businesses = JSON.stringify(data.businesses[0]);
+			_businesses = JSON.stringify(data.businesses[0].name);
 			});
 			for (i=0; i<_count; i++)
 			{
