4720bcaa531dee609692bf528878013cf80c0c97
Thomas Bernfeld <happpyier@gmail.com>
2016-11-14 08:33:14 -0500

    Work Yelpier

--- index.js
+++ index.js
@@ -99,7 +99,7 @@ app.get([''], function(request, response)
 			}
 			for (i=0; i<20; i++)
 			{
-				response.write("<div> <img src='" + _buis_image_url_Array[i] + "'> </img>" + _buis_name_Array[i] + "..." + _buis_snippet_Array[i] + "</div>");
+				response.write("<div> <img src='" + _buis_image_url_Array[i] + "'> </img>" + _buis_name_Array[i] + "<br/>" + _buis_snippet_Array[i] + "</div>");
 			 }
 
 			response.write(data);
