fe3c53372fb39500b823790b405bc175a5d7022d
Thomas Bernfeld <happpyier@gmail.com>
2016-11-16 12:24:19 -0500

    Setting up the Tickers

--- index.js
+++ index.js
@@ -6,6 +6,8 @@ const fs = require('fs');
 var path = require("path");
 var url = require("url");
 var _screen_name;
+var ticker;
+var alertVar;
 app.set('port', (process.env.PORT || 5000));
 app.set("Content-Type", "text/html");
 app.get([''], function(request, response) {
@@ -19,54 +21,15 @@ var postSqlVar = "SELECT * FROM stock_table";
 		  else
 		   { 
 				alertVar = result.rows;
-				// randid_vote = "";
-				// votechoose_vote = "";
-				// votes_vote = "";
-				// uservoted_vote = "";
-				// ipvoted_vote = "";
-				// title_vote = "";
-				// votedalready = "";
-				// alertVar.forEach(function(value)
+				ticker = "";
+				alertVar.forEach(function(value)
 				{
-					// randid_vote = value["randid"];
-					// votechoose_vote = votechoose_vote +  value["votechoose"]  + "|";
-					// votes_vote = votes_vote +  value["votes"]  + "|";
-					// uservoted_vote = uservoted_vote +  value["uservoted"]  + "|";
-					// ipvoted_vote = ipvoted_vote +  value["ipvoted"]  + "|";
-					// title_vote = value["title"] ;
-					// votedalready = votedalready +  value["votedalready"]  + "|";
+					ticker = ticker +  value["ticker"]  + "|";
 				});
-				// votechoose_vote = votechoose_vote.substring(0, votechoose_vote.length - 1);
-				// votes_vote = votes_vote.substring(0, votes_vote.length - 1);
-				// uservoted_vote = uservoted_vote.substring(0, uservoted_vote.length - 1);
-				// ipvoted_vote = ipvoted_vote.substring(0, ipvoted_vote.length - 1);
-				// votedalready = votedalready.substring(0, votedalready.length - 1);			
-				// response.write( "<div class='hidden' style='display:none' id= 'randid_hidden'>" + randid_vote + "</div> <div class='hidden' style='display:none' id= 'votechoose_hidden'>" + votechoose_vote + "</div> <div class='hidden' style='display:none' id= 'votes_hidden'>" + votes_vote + "</div> <div class='hidden' style='display:none' id= 'uservoted_hidden'>" + uservoted_vote + "</div> <div class='hidden' style='display:none' id= 'ipvoted_hidden'>" + ipvoted_vote + "</div> <div class='hidden' style='display:none' id= 'title_hidden'>" + title_vote + "</div> <div class='hidden' style='display:none' id= 'alreadyvoted'>" + votedalready + "</div>"	);
+				ticker = ticker.substring(0, ticker.length - 1);
+				response.write( "<div class='hidden' style='display:none' id= 'ticker_hidden'>" + ticker + "</div>");
 		   }
 		   done();
-			// if (_screen_name.length > 0)
-			// {
-				// fs.readFile('thispollSignedIn.html', 'utf8', function (err,data) {
-					// if (err) 
-					// {
-						// return console.log(err);
-					// }
-					// response.write(data);
-					// response.end();
-				// });
-			
-			// }
-			// else
-			// {
-				// fs.readFile('thispoll.html', 'utf8', function (err,data) {
-					// if (err) 
-					// {
-						// return console.log(err);
-					// }
-					// response.write(data);
-					// response.end();
-				// });
-			// }
 		});
 
 	});
