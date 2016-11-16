5b14aa99b5ce14b29fd0393672d697455165362a
Thomas Bernfeld <happpyier@gmail.com>
2016-11-16 09:20:27 -0500

    Setting up Initial Files

--- index.js
+++ index.js
@@ -5,10 +5,9 @@ const https = require('https');
 const fs = require('fs');
 var path = require("path");
 var url = require("url");
-app.set('port', (process.env.PORT || 5000));
-app.use(cookieParser());
-app.set("Content-Type", "text/html");
 var _screen_name;
+app.set('port', (process.env.PORT || 5000));
+app.set("Content-Type", "text/html");
 app.get([''], function(request, response) {
 	_screen_name = "";
 	resposne.write("Home Page");
