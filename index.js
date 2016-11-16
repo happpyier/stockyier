6290bf0c743e19e2f6e2023e26c2fde61a10ff90
Thomas Bernfeld <happpyier@gmail.com>
2016-11-16 09:03:14 -0500

    Setting up initial Files

--- index.js
+++ index.js
@@ -5,11 +5,14 @@ const https = require('https');
 const fs = require('fs');
 var path = require("path");
 var url = require("url");
+app.set('port', (process.env.PORT || 5000));
 app.use(cookieParser());
 app.set("Content-Type", "text/html");
+var _screen_name;
 app.get(['/'], function(request, response) {
 	_screen_name = "";
-	response.end("Home Page");
+	resposne.write("Home Page");
+	response.end();
 });
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port')); 
