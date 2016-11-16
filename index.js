2ca73f1e3ecb44fedd61719f8af35e3e93e095f6
Thomas Bernfeld <happpyier@gmail.com>
2016-11-16 08:57:28 -0500

    Setting up initial Files

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ var path = require("path");
 var url = require("url");
 app.use(cookieParser());
 app.set("Content-Type", "text/html");
-app.get([''], function(request, response) {
+app.get(['/'], function(request, response) {
 	_screen_name = "";
 	response.end("Home Page");
 });
