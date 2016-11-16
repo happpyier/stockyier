5686725417d5221bd84ce519f96cb939dce62c68
Thomas Bernfeld <happpyier@gmail.com>
2016-11-16 08:31:02 -0500

    Setting up Files

---
+++ index.js
@@ -0,0 +1,15 @@
+var express = require('express'); 
+var app = express();
+var pg = require('pg');
+const https = require('https');
+const fs = require('fs');
+var path = require("path");
+var url = require("url");
+
+app.set('port', (process.env.PORT || 5000));
+app.set("Content-Type", "text/html");
+
+app.listen(app.get('port'), function() {
+//
+  console.log('Node app is running on port', app.get('port')); 
+});
