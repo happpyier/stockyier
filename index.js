7339415eea02004263594b97063984e9c0b5a314
Thomas Bernfeld <happpyier@gmail.com>
2016-11-17 12:17:34 -0500

    Stock Stuffs

--- index.js
+++ index.js
@@ -10,6 +10,9 @@ var _screen_name;
 var ticker;
 var tickerName = "";
 var alertVar;
+var graphDataElement = {};
+var graphDataArray = [];
+var graphDataArrayEncoded;
 app.set('port', (process.env.PORT || 5000));
 app.set("Content-Type", "text/html");
 //http://dev.markitondemand.com/MODApis/Api/v2/Quote?symbol=FB API to use for market info.
@@ -27,15 +30,29 @@ app.get([''], function(request, response) {
 					markit.getQuote(ticker, function(err, data) {
 						// Where data is an array of stock symbols 
 						tickerName = data.Name;
+						graphDataElement.Normalized = "false";
+						var d = new Date(new Date(0,0,0,0,0,0).setFullYear(new Date().getFullYear()));
+						graphDataElement.StartDate = (d.getFullYear()-1) +"-"+d.getMonth()+"-"+d.getDay()+"00:00:00-00";
+						graphDataElement.EndDate = d.getFullYear() +"-"+d.getMonth()+"-"+d.getDay()+"00:00:00-00";
+						graphDataElement.NumberOfDays = 12;
+						graphDataElement.DataPeriod = "Month";
+						graphDataElement.LabelPeriod = "Month";
+						graphDataElement.LabelInterval = 1;
+						graphDataElement.Elements = [ Symbol: tickerName, Type: "price", Params: null ]
+						graphDataElement.Name = data.Name;
+						graphDataArray.push(graphDataElement);
+						graphDataArrayEncoded = JSON.stringify(graphDataArray)
 					});
 					response.write("<div class='ticker'> <boldHeader>" + ticker + "</boldHeader> <br/><br/>" + tickerName + "(" + ticker + ") Prices, 	Dividends, Splits and Trading Volume </div>");
 				});
 			done();
+			//set iframe with id="graph_results" to http://dev.markitondemand.com/Api/v2/InteractiveChart?parameters=graphDataArrayEncoded A JSON encoded InteractiveChartDataInput object
 			fs.readFile('index2.html', 'utf8', function (err,data) {
 				if (err) 
 				{
 					return console.log(err);
 				}
+				response.write("<div style='display:none;' id='graphDataArrayEncoded_hidden'>" + graphDataArrayEncoded + "</div>");
 				response.end(data);
 			});
 		});
