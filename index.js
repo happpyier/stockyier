b9256c7b2f13ca5df3fc243452d383251c865c6f
Thomas Bernfeld <happpyier@gmail.com>
2016-11-17 10:33:27 -0500

    Stock Stuffs

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const https = require('https');
 const fs = require('fs');
 var path = require("path");
 var url = require("url");
-var markit = require('node-markitondemand');
+const Markit = require('markit-on-demand');
 var _screen_name;
 var ticker;
 var tickerName = "";
