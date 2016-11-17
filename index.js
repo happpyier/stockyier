1d64c62f4ec27e91978a11dc03bab41f2a246807
Thomas Bernfeld <happpyier@gmail.com>
2016-11-17 11:15:52 -0500

    Stock Stuffs

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ app.set("Content-Type", "text/html");
 app.get([''], function(request, response) {
 	markit.getQuote('AAPL', function(err, data) {
      // Where data is an array of stock symbols 
-     tickerName = JSON.stringify(data.Name);
+     tickerName = data.Name;
    });
 	var queryForSQL = "SELECT * FROM stock_table";
 	fs.readFile('index.html', 'utf8', function (err,data) {
