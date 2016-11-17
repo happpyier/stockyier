40ed5e1b81cbea46bfc16af844e6abc48ed440b3
Thomas Bernfeld <happpyier@gmail.com>
2016-11-17 10:37:20 -0500

    Stock Stuffs

--- index.js
+++ index.js
@@ -14,10 +14,10 @@ app.set('port', (process.env.PORT || 5000));
 app.set("Content-Type", "text/html");
 //http://dev.markitondemand.com/MODApis/Api/v2/Quote?symbol=FB API to use for market info.
 
-markit.getQuote('AAPL', function(err, stock) {
- // Where stock is an object of stock data 
- //console.log(stock);
-});
+Markit.getQuote('FB')
+    .then(( res ) => {
+        tickerName = typeof(res)
+    });
 app.get([''], function(request, response) {
 	var queryForSQL = "SELECT * FROM stock_table";
 	fs.readFile('index.html', 'utf8', function (err,data) {
