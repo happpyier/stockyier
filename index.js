189d2a1d2e0772d9c347d54ba059a4f213bfbcb9
Thomas Bernfeld <happpyier@gmail.com>
2016-11-17 16:12:49 -0500

    stock stuff

--- index.js
+++ index.js
@@ -38,7 +38,7 @@ app.get([''], function(request, response) {
 						graphDataElement.LabelInterval = 1;
 						graphDataElementName = ticker;
 						graphDataElement.Elements = [];
-						graphDataElement.Elements.push({'"Symbol":"'+graphDataElementName+'","Type":"price","Params":"[c]"'});
+						graphDataElement.Elements.push('{"Symbol":"'+graphDataElementName+'","Type":"price","Params":"[c]"}');
 						pregraphDataArrayEncoded = JSON.stringify(graphDataElement);
 						graphDataArrayEncoded = encodeURI(pregraphDataArrayEncoded);
 					});
