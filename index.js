d6be7103463b6fde8ebaefb7ecb4acfa0dd57288
Thomas Bernfeld <happpyier@gmail.com>
2016-11-22 11:40:52 -0500

    Frames

--- index.js
+++ index.js
@@ -14,7 +14,6 @@ var tickerId = "";
 var tickerStatus = "";
 var alertVar;
 var graphDataElement = {};
-//var graphDataElement.Elements = [];
 var graphDataArray = [];
 var tempDataArray = {};
 var pregraphDataArrayEncoded;
@@ -30,6 +29,7 @@ app.get([''], function(request, response) {
 	pg.connect(process.env.DATABASE_URL, function(err, client, done) {
 		client.query(queryForSQL, function(err, result) {
 				testSQlValue = result.rows;
+				graphDataElement.Elements = [];
 				testSQlValue.forEach(function(value){
 					ticker = value["ticker"];
 					tickerName = value["title"];
