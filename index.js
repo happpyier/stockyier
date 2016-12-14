3a86194e36de3ed200359f9692b3f301423dc3ee
Thomas Bernfeld <happpyier@gmail.com>
2016-12-14 17:09:05 -0500

    Elements

--- index.js
+++ index.js
@@ -43,7 +43,7 @@ app.get([''], function(request, response) {
 					tempDataArray.Type = "price";
 					tempDataArray.Params = ["c"];
 					graphDataElement.Elements.push(tempDataArray);
-					response.write("<div class='tempDataArrayVal'> " + ticker + "</div>");
+					response.write("<div class='tempDataArrayVal hidden'> " + ticker + "</div>");
 					graphDataElement.Elements.push(tempDataArray);
 					response.write("<div class='ticker'> <boldHeader >" + ticker + "</boldHeader> <button class='borderless' onclick="+"removeTicker('"+ticker+"')"+">x</button> <br/><br/>" + tickerName + "(" + ticker + ") Prices, 	Dividends, Splits and Trading Volume </div>");
 					if (h == (testSQlValue.length-1))
