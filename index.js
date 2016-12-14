cc75f824e5ea826c25216b99df6d919b456a3dcd
Thomas Bernfeld <happpyier@gmail.com>
2016-12-14 16:42:32 -0500

    not sure where

--- index.js
+++ index.js
@@ -48,14 +48,13 @@ app.get([''], function(request, response) {
 					if (h == (testSQlValue.length-1))
 					{
 						
-						// var x = document.getElementsByClassName("tempDataArrayVal");
-						// var j;
-						// for (j = 0; j < x.length; j++) {
-							// tempDataArray.Symbol = x[j].innerHTML;
-							// tempDataArray.Type = "price";
-							// tempDataArray.Params = ["c"];
-							// graphDataElement.Elements.push(tempDataArray);
-						// }
+						var x = document.getElementsByClassName("tempDataArrayVal");
+						var j;
+						for (j = 0; j < x.length; j++) {
+							graphDataElement.Elements.Symbol = x[j].innerHTML;
+							graphDataElement.Elements.Type = "price";
+							graphDataElement.Elements.Params = ["c"];
+						}
 						graphDataArrayEncoded = JSON.stringify(graphDataElement);
 						response.write("<div style='display:block;' id='graphDataArrayEncoded_hidden'>" + graphDataArrayEncoded + "</div>");
 					}					
