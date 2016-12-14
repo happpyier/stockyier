0c79440ec4b914e17edfd2f9e4c7dc960465e680
Thomas Bernfeld <happpyier@gmail.com>
2016-12-14 16:57:08 -0500

    not sure where

--- index.js
+++ index.js
@@ -44,6 +44,11 @@ app.get([''], function(request, response) {
 					//tempDataArray.Params = ["c"];
 					//graphDataElement.Elements.push(tempDataArray);
 					response.write("<div class='tempDataArrayVal'> " + ticker + "</div>");
+					var x = document.getElementsByClassName("tempDataArrayVal");
+					tempDataArray.Symbol = x[h].innerHTML;
+					tempDataArray.Type = "price";
+					tempDataArray.Params = ["c"];
+					graphDataElement.Elements.push(tempDataArray);
 					response.write("<div class='ticker'> <boldHeader >" + ticker + "</boldHeader> <button class='borderless' onclick="+"removeTicker('"+ticker+"')"+">x</button> <br/><br/>" + tickerName + "(" + ticker + ") Prices, 	Dividends, Splits and Trading Volume </div>");
 					if (h == (testSQlValue.length-1))
 					{
