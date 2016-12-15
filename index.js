1356bab118d1a3ae48cf22cc7d813f09ed3e37c0
Thomas Bernfeld <happpyier@gmail.com>
2016-12-15 12:14:32 -0500

    Loading the Elements

--- index.js
+++ index.js
@@ -40,22 +40,22 @@ app.get([''], function(request, response) {
 					graphDataElement.DataPeriod = "Day";
 					graphDataElement.LabelPeriod = "Month";
 					response.write("<div class='tempDataArrayVal'>" + ticker + "</div>");
-					// tempDataArray.Symbol += ticker;
-					// tempDataArray.Type = "price";
-					// tempDataArray.Params = ["c"];
-					// graphDataElement.Elements.push(tempDataArray);
+					tempDataArray.Symbol += ticker;
+					tempDataArray.Type = "price";
+					tempDataArray.Params = ["c"];
+					graphDataElement.Elements.push(tempDataArray);
 					response.write("<div class='ticker'> <boldHeader >" + ticker + "</boldHeader> <button class='borderless' onclick="+"removeTicker('"+ticker+"')"+">x</button> <br/><br/>" + tickerName + "(" + ticker + ") Prices, 	Dividends, Splits and Trading Volume </div>");
 					if (h == (testSQlValue.length-1))
 					{
 						
 						var x = document.getElementsByClassName("tempDataArrayVal");
 						var j;
-						for (j = 0; j < x.length; j++) {
-							tempDataArray.Symbol = x[j].innerHTML;
-							tempDataArray.Type = "price";
-							tempDataArray.Params = ["c"];
-							graphDataElement.push("{'Symbol':'"+x[j].innerHTML+"'price','Params':['c']}");
-						}
+						// for (j = 0; j < x.length; j++) {
+							// tempDataArray.Symbol = x[j].innerHTML;
+							// tempDataArray.Type = "price";
+							// tempDataArray.Params = ["c"];
+							// graphDataElement.push("{'Symbol':'"+x[j].innerHTML+"'price','Params':['c']}");
+						// }
 						graphDataElement.Elements.push(tempDataArray);
 						graphDataArrayEncoded = JSON.stringify(graphDataElement);
 						response.write("<div style='display:block;' id='graphDataArrayEncoded_hidden'>" + graphDataArrayEncoded + "</div>");
