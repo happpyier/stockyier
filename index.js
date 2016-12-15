c6a3555496c04d9f49f2165c1523d00df3b1272d
Thomas Bernfeld <happpyier@gmail.com>
2016-12-15 15:29:15 -0500

    Loading the Elements

--- index.js
+++ index.js
@@ -46,7 +46,7 @@ app.get([''], function(request, response) {
 					sub_array.push(ticker);
 					preSuperArrayVal = "{'Symbol':'"+sub_array[0]+"','Type':'price','Params':['c']}";
 					SuperArrayVal = preSuperArrayVal.substring(1, preSuperArrayVal.length - 1);
-					graphDataElement.Elements.push(preSuperArrayVal);
+					graphDataElement.Elements.push(SuperArrayVal);
 					// response.write("<div class='tempDataArrayVal'>" + ticker + "</div>");
 					// response.write("SUPER ARRAY...." + h + sub_array + "....SUPER ARRAY" + h);	
 					sub_array.shift();
