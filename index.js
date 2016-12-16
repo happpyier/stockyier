dfb57aa92f4f44d5812f2febbef53da85e4c34d3
Thomas Bernfeld <happpyier@gmail.com>
2016-12-16 15:11:01 -0500

    Still playin with elements

--- index.js
+++ index.js
@@ -45,9 +45,9 @@ app.get([''], function(request, response) {
 					graphDataElement.LabelPeriod = "Month";
 					sub_array.push(ticker);
 					//preSuperArrayVal = "{'Symbol':'"+sub_array[0]+"','Type':'price','Params':['c']}";
-					var tempSymbol = '{\'Symbol\':\''+sub_array[0]+'\',';
-					var tempType = '\'Type\':\'price,';
-					var tempParams = '\'Params\':[\'c\']}';
+					var tempSymbol = JSON.parse('{\'Symbol\':\''+sub_array[0]+'\',');
+					var tempType = JSON.parse('\'Type\':\'price,');
+					var tempParams = JSON.parse('\'Params\':[\'c\']}');
 					SuperArrayVal = tempSymbol+tempType+tempParams;
 					graphDataElement.Elements.push(SuperArrayVal);
 					// response.write("<div class='tempDataArrayVal'>" + ticker + "</div>");
